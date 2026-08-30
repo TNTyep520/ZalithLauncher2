e418288aa6f6ba19cb9680e69678090d92aabd88
TNTyep520 <sbtntyep520@gmail.com>
2026-08-30 13:48:22 +0800

    chore(library_info): sdl2-compat 与 SDL3 依赖库

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/library/_Libraries.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/library/_Libraries.kt
@@ -76,6 +76,8 @@ val libraryData = listOf(
     LibraryInfo("Process Phoenix", "Copyright © 2015 Jake Wharton", LICENSE_APACHE_2, "https://github.com/JakeWharton/ProcessPhoenix"),
     LibraryInfo("proxy-client-android", null, License(LICENSE_LGPL_3, R.raw.lgpl_3_license), "https://github.com/TouchController/TouchController"),
     LibraryInfo("Reorderable", "Copyright © 2023 Calvin Liang", LICENSE_APACHE_2, "https://github.com/Calvin-LL/Reorderable"),
+    LibraryInfo("sdl2-compat", "Copyright (C) 2026 Sam Lantinga <slouken@libsdl.org>", License("Zlib", R.raw.sdl2_compat_license), "https://github.com/libsdl-org/sdl2-compat"),
+    LibraryInfo("SDL3", "Copyright (C) 1997-2026 Sam Lantinga <slouken@libsdl.org>", License("Zlib", R.raw.sdl3_license), "https://github.com/libsdl-org/SDL"),
     LibraryInfo("skinview3d", "Copyright © 2014-2018 Kent Rasmussen; Copyright © 2017-2022 Haowei Wen, Sean Boult and contributors", License(LICENSE_MIT, R.raw.skinview3d_license), "https://github.com/bs-community/skinview3d"),
     LibraryInfo("sora-editor", "Copyright (C) 2020-2026  Rosemoe", License("LGPL-2.1 License", R.raw.sora_editor_license), "https://github.com/Rosemoe/sora-editor"),
     LibraryInfo("Terracotta", "Copyright © 2025 Burning_TNT", License(LICENSE_AGPL_3, net.burningtnt.terracotta.R.raw.terracotta_license), "https://github.com/burningtnt/Terracotta"),
